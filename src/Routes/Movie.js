f0fdd917ff4bf7d0432db1bdf652bfc155f40f6f
william <cloudnhs@gmail.com>
2020-09-21 20:31:54 +0900

    networking #1

--- src/Routes/Movie.js
+++ src/Routes/Movie.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {movieApi} from "../api";
+
+
 
 const Movie = () => {
+
+    const [movies, setMovies] = useState({
+        results: [],
+        error: null
+
+    });
+
+    const getData = async () => {
+        const [results, error] = await movieApi.nowPlaying()
+        setMovies({
+            results: results,
+            error: error
+        })
+        console.log(results)
+    }
+
+    useEffect(() => {
+        console.log("test");
+        getData()
+    }, [])
+
     return (
         <div>
-            <h1>Movie</h1>
+            <h1>Movie is {movies.results.length}</h1>
         </div>
     );
 };
--- src/Routes/TV.js
+++ src/Routes/TV.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {tvApi} from "../api"
 
 const TV = () => {
+
+    const [tv, setTv] = useState({
+        results: [],
+        error: null
+
+    });
+
+    const getData = async () => {
+        const [results, error] = await tvApi.onTheAir()
+        setTv({
+            results: results,
+            error: error
+        })
+        console.log(results)
+    }
+
+    useEffect(() => {
+        console.log("test");
+        getData()
+    }, [])
+
     return (
         <div>
-            <h1>TV</h1>
+            <h1>TV is {tv.results.length}</h1>
         </div>
     );
 };
