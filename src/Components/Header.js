2acf2377f9e2f6fb68486d31b3a7eaffb8262adb
william <cloudnhs@gmail.com>
2020-09-21 20:02:48 +0900

    api 정리

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
+
 
 const Head = styled.header`
     color: white;
@@ -27,6 +28,9 @@ const Item = styled.li`
     text-align: center;
     font-size: 16px;
     font-weight: 600;
+    border-bottom: 5px solid
+        ${(props) => (props.current ? "#e01f1f" : "transparent")};
+    transition: border-bottom 0.5s ease-in-out;    
 `;
 
 const SLink = styled(Link)`
@@ -36,24 +40,24 @@ const SLink = styled(Link)`
     justify-content: center;
 `;
 
-const Header = () => {
+const Header = withRouter(({ location: {pathname} }) => {
     return (
         <Head>
             <List>
-                <Item>
+                <Item current={pathname === "/"}>
                     <SLink to="/">Movies</SLink>
                 </Item>
 
-                <Item>
+                <Item current={pathname === "/tv"}>
                     <SLink to="/tv">TV</SLink>
                 </Item>
 
-                <Item>
+                <Item current={pathname === "/search"}>
                     <SLink to="/search">Search</SLink>
                 </Item>
             </List>
         </Head>
     );
-};
+});
 
 export default Header;
---
+++ src/api.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const TMDB_KEY = "bed463794a5f3600a36b595cd81ecfee";
+
+const makeRequest = (path, params) =>
+    axios.get(`https://api.themoviedb.org/3/${path}`, {
+        params: {
+            ...params,
+            api_key: TMDB_KEY
+        }
+    })
+
+const getAnything = async (path, params = {}) => {
+    try {
+        const {
+            data: {results },
+            data
+        } = await makeRequest(path, params)
+        return [results || data, null];
+    } catch(e) {
+        console.log(e);
+        return [null, e];
+    }
+
+}
+
+export const movieApi = {
+    nowPlaying: () => getAnything("movie/now_playing"),
+    popular: () => getAnything("movie/popular"),
+    topRated: () => getAnything("movie/top_rated"),
+    upcoming: () => getAnything("movie/upcoming"),
+    detail: (movie_id) => getAnything(`movie/${movie_id}`),
+    search: (query) => getAnything("search/movie", { query }),
+    similar: (movie_id) => getAnything(`movie/${movie_id}/similar`),
+    videos: (movie_id) => getAnything(`movie/${movie_id}/videos`)
+
+}
+
+export const tvApi = {
+    airingToday: () => getAnything("tv/airing_today"),
+    onTheAir: () => getAnything("tv/on_the_air"),
+    popular: () => getAnything("tv/popular"),
+    topRated:() => getAnything("tv/top_rated"),
+    detail: (tv_id) => getAnything(`tv/${tv_id}`),
+    search: (query) => getAnything("search/tv", { query }),
+    similar: (tv_id) => getAnything(`tv/${tv_id}/similar`),
+    videos: (tv_id) => getAnything(`tv/${tv_id}/videos`)
+}
